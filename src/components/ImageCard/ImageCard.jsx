96d9eb58184949fb2c02d2f579321cc74e4dc4bb
ArtemBlyshchyk <artem.bl31@gmail.com>
2024-04-01 11:58:51 +0300

    added event onClick on the img

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,7 +1,11 @@
 const ImageCard = ({ photo, openModal }) => {
   return (
-    <div onClick={() => openModal(photo)}>
-      <img src={photo.urls.small} alt={photo.alt_description} />
+    <div>
+      <img
+        src={photo.urls.small}
+        alt={photo.alt_description}
+        onClick={() => openModal(photo)}
+      />
     </div>
   );
 };
